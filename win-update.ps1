57dbc716a4fa9afe33f846491ea62ca695535fe3
MinhQuan Tran <minhquan.tran@wsu.edu>
2021-02-25 09:35:33 -0800

    Added script to be tracked

---
+++ win-update.ps1
@@ -0,0 +1,13 @@
+﻿#Requires -RunAsAdministrator
+if (Get-Module -ListAvailable -Name PSWindowsUpdate) {
+    Get-Package -Name PSWindowsUpdate
+    Update-Module -Name PSWindowsUpdate
+} 
+else {
+    Install-PackageProvider -Name NuGet -Force
+    Install-Module -Name PSWindowsUpdate -Force
+}
+Get-Package -Name PSWindowsUpdate
+Get-WindowsUpdate
+#Add-WUServiceManager -MicrosoftUpdate -AcceptAll
+#Install-WindowsUpdate -MicrosoftUpdate -AcceptAll
