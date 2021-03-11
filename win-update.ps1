e43509769212f025704b4a2e044648373aef51cc
MinhQuan Tran <minhquan.tran@wsu.edu>
2021-03-11 10:50:26 -0800

    Implemented ability to read a single kb to update

--- win-update.ps1
+++ win-update.ps1
@@ -10,32 +10,28 @@ param(
     [string]$kb
 )
 
-if($kb){
-    Write-Host "`r`nkb value given!`r`n"
-    Write-Host $kb
-} else {
-    Write-Host "No kb value given!"
+# Check if PSWindowsUpdate exists, update if it is
+if (Get-Module -ListAvailable -Name PSWindowsUpdate) {
+    Write-Host "Update module if available..."
+    Update-Module -Name PSWindowsUpdate
+} 
+else {
+    Install-PackageProvider -Name NuGet -Force
+    Install-Module -Name PSWindowsUpdate -Force
 }
+Get-Package -Name PSWindowsUpdate
 
-# # Check if PSWindowsUpdate exists, update if it is
-# if (Get-Module -ListAvailable -Name PSWindowsUpdate) {
-#     Write-Host "Update module if available..."
-#     Update-Module -Name PSWindowsUpdate
-# } 
-# else {
-#     Install-PackageProvider -Name NuGet -Force
-#     Install-Module -Name PSWindowsUpdate -Force
-# }
-# Get-Package -Name PSWindowsUpdate
-
-# # Begin picking up updates and install them
-# Write-Host "`r`nUpdating..."
-# Download-WindowsUpdate -MicrosoftUpdate -AcceptAll
-# if ($restart) {
-#     # Restart flag specified, allow reboot
-#     Install-WindowsUpdate -MicrosoftUpdate -AcceptAll
-# }
-# else {
-#     # (Default) Delay reboot
-#     Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
-# }
+# Begin picking up updates and install them
+Write-Host "`r`nUpdating..."
+if ($kb) {
+    Download-WindowsUpdate -MicrosoftUpdate -AcceptAll -KBArticleID $kb
+}
+else {
+    Download-WindowsUpdate -MicrosoftUpdate -AcceptAll
+}
+if ($restart) {
+    Install-WindowsUpdate -MicrosoftUpdate -AcceptAll
+}
+else {
+    Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
+}
