2e7882dacb059f3c4d90edb20403f608fc4b0b16
MinhQuan Tran <minhquan.tran@wsu.edu>
2021-03-23 10:12:00 -0700

    Implemented restart delay, needs to be tested.

--- win-update.ps1
+++ win-update.ps1
@@ -5,9 +5,11 @@
 # Specify available params:
 #   - restart: restart the computer after the updates are installed
 #   - kb: string of kb values to pass to the script
+#   - time: restart the computer after x amount of hours, must be used with restart
 param(
     [Parameter(Mandatory = $false)][switch]$restart,
-    [string[]]$kb
+    [string[]]$kb,
+    [string[]]$time
 )
 
 # Check if PSWindowsUpdate exists, update if it is
@@ -21,11 +23,27 @@ else {
 }
 Get-Package -Name PSWindowsUpdate
 
-# Begin picking up updates and install them
+# Begin picking up updates and download them
 Write-Host "`r`nUpdating..."
 Download-WindowsUpdate -MicrosoftUpdate -AcceptAll
+
+# Based on given command-line args, restart the computer
 if ($restart) {
-    if ($kb) {
+    # Restart flag specified, check $time if delay is specified
+    if ($time) {
+        # 1 hour = 3600 seconds
+        if ($kb) {
+            Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot -KBArticleID $kb
+            Start-Sleep -Seconds ([decimal]$time * 3600)
+            Restart-Computer
+        }
+        else {
+            Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
+            Start-Sleep -Seconds ([decimal]$time * 3600)
+            Restart-Computer
+        }
+    }
+    elseif ($kb) {
         Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -KBArticleID $kb
     }
     else {
@@ -33,6 +51,7 @@ if ($restart) {
     }
 }
 else {
+    # No restart flag specified
     if ($kb) {
         Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot -KBArticleID $kb
     }
